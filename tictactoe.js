2a93c3df6076fbcda8a63aecb42086265b328e58
Jim Medlock <jdmedlock@gmail.com>
2016-12-01 19:43:48 -0600

    added basic code for inital test

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,26 @@
 //
-// File Name: index.html
+// File Name: tictactoe.js
 // Date: 11/29/2016
 // Programmer: Jim Medlock
 //
+
+// -------------------------------------------------------------
+// Initialization function(s)
+// -------------------------------------------------------------
+
+// Initialization Logic invoked when the DOM is ready for execution
+//
+// Input parameters: N/a
+// Returns: N/a
+$(document).ready(function() {
+  console.clear();
+
+   // Create a button handler to decrement the break interval length
+  $(".pt-btn-break-minus").click(function(event) {
+  });
+
+});
+
+// -------------------------------------------------------------
+// User Interface functions
+// -------------------------------------------------------------
