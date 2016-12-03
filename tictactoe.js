b219bdef92b9a9773966631add00c3ec32696c8d
Jim Medlock <jdmedlock@gmail.com>
2016-12-03 08:48:09 -0600

    removed extra brace from claimCell function definition

--- tictactoe.js
+++ tictactoe.js
@@ -12,13 +12,12 @@
 //
 // Returns: N/a
 $(document).ready(function() {
-  console.clear();
+   console.clear();
 
-  // Create a button handler to decrement the break interval length
-  $(".pt-btn-break-minus").click(function(event) {
-  });
+   // Create a button handler to decrement the break interval length
+   $(".pt-btn-break-minus").click(function(event) {});
 
-  claimCell("X","t3-canvas-1");
+   claimCell("X", "t3-canvas-1");
 
 });
 
@@ -34,29 +33,35 @@ $(document).ready(function() {
 // Initialization Logic invoked when the DOM is ready for execution
 //
 // Returns: N/a
-function claimCell(charToPlace, canvasName) {}
-  var ctx = document.querySelector(canvasName).getContext("2d"),
-      dashLen = 220, dashOffset = dashLen, speed = 5,
-      txt = charToPlace, x = 30, i = 0;
+function claimCell(charToPlace, canvasName) {
+   var ctx = document.querySelector(canvasName).getContext("2d"),
+      dashLen = 220,
+      dashOffset = dashLen,
+      speed = 5,
+      txt = charToPlace,
+      x = 30,
+      i = 0;
 
-  ctx.font = "50px Comic Sans MS, cursive, TSCu_Comic, sans-serif";
-  ctx.lineWidth = 5; ctx.lineJoin = "round"; ctx.globalAlpha = 2/3;
-  ctx.strokeStyle = ctx.fillStyle = "#1f2f90";
+   ctx.font = "50px Comic Sans MS, cursive, TSCu_Comic, sans-serif";
+   ctx.lineWidth = 5;
+   ctx.lineJoin = "round";
+   ctx.globalAlpha = 2 / 3;
+   ctx.strokeStyle = ctx.fillStyle = "#1f2f90";
 
-  (function loop() {
-    ctx.clearRect(x, 0, 60, 150);
-    ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
-    dashOffset -= speed;                                         // reduce dash length
-    ctx.strokeText(txt[i], x, 90);                               // stroke letter
+   (function loop() {
+      ctx.clearRect(x, 0, 60, 150);
+      ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
+      dashOffset -= speed; // reduce dash length
+      ctx.strokeText(txt[i], x, 90); // stroke letter
 
-    if (dashOffset > 0) requestAnimationFrame(loop);             // animate
-    else {
-      ctx.fillText(txt[i], x, 90);                               // fill final letter
-      dashOffset = dashLen;                                      // prep next char
-      x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
-      ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());        // random y-delta
-      ctx.rotate(Math.random() * 0.005);                         // random rotation
-      if (i < txt.length) requestAnimationFrame(loop);
-    }
-  })();
+      if (dashOffset > 0) requestAnimationFrame(loop); // animate
+      else {
+         ctx.fillText(txt[i], x, 90); // fill final letter
+         dashOffset = dashLen; // prep next char
+         x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
+         ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random()); // random y-delta
+         ctx.rotate(Math.random() * 0.005); // random rotation
+         if (i < txt.length) requestAnimationFrame(loop);
+      }
+   })();
 }
